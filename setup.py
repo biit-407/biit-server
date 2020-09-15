a76d528b5f9f575639291a872328294acb7ac772
Ryan Chen <ryanjchen2@gmail.com>
2020-09-15 15:18:44 -0400

    Adding a setup.py file for testing

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='biit-server',
+    version='420.6.9',
+    url='https://biit.com',
+    description='The basic blog app built in the Flask tutorial.',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'coverage',
+        ],
+    },
+)
