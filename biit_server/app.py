a3721f6969719626f093e772a1f9357e41817330
Lucas <lucashahn@comcast.net>
2020-09-15 16:48:47 -0400

    Added Post Endpoint validation

--- biit_server/app.py
+++ biit_server/app.py
@@ -6,6 +6,9 @@ import json
 
 app = Flask(__name__)
 
+def missing_data(missedParam, response):
+    return "Missing Parameter "+ missedParam, response
+
 
 @app.route("/test")
 def test_route():
@@ -15,10 +18,20 @@ def test_route():
 @app.route("/account", methods=["POST", "GET", "PUT", "DELETE"])
 def account_route():
     if request.method == "POST":
-        data = json.loads(request.get_json())
+        data = ""
+        try:
+            data = json.loads(request.get_json())
+        except:
+            return "Failed to load account JSON", 400
         if "token" not in data or not data["token"]:
-            return "Token not found", 400
-        
+            return missing_data("token",400)
+        if "fname" not in data or not data["fname"]:
+            return missing_data("fname",400)
+        if "lname" not in data or not data["lname"]:
+            return missing_data("lname",400)
+        if "email" not in data or not data["email"]:
+            return missing_data("email",400)
+        #TODO @Ryan Create the DB stuff
         if account.create(data):
             return "Account Created", 200
         return "Failed to create account", 400
