192f21c9091884c2018d0611a84d1100d4cc44b4
Lucas <lucashahn@comcast.net>
2020-09-17 15:34:46 -0400

    Updated key checking to be array

--- biit_server/app.py
+++ biit_server/app.py
@@ -16,19 +16,15 @@ def create_app():
     @app.route("/account", methods=["POST", "GET", "PUT", "DELETE"])
     def account_route():
         if request.method == "POST":
+            keys = ["token", "fname", "lname", "email"]
             data = ""
             try:
                 data = json.loads(request.get_json())
             except:
                 return "Failed to load account JSON", 400
-            if "token" not in data or not data["token"]:
-                return missing_data("token", 400)
-            if "fname" not in data or not data["fname"]:
-                return missing_data("fname", 400)
-            if "lname" not in data or not data["lname"]:
-                return missing_data("lname", 400)
-            if "email" not in data or not data["email"]:
-                return missing_data("email", 400)
+            for key in keys:
+                if key not in data or not data[key]:
+                    return missing_data(key, 400)
             # TODO @Ryan Create the DB stuff
             if account.create(data):
                 return "Account Created", 200
@@ -43,27 +39,26 @@ def create_app():
                 return missing_data("email", 400)
             return account.get(data)
         elif request.method == "PUT":
+            keys = ["token", "email"]
             data = ""
             try:
                 data = json.loads(request.get_json())
             except:
-                return "Failed to load JSON object", 400
-            if "token" not in data or not data["token"]:
-                return missing_data("token", 400)
-            if "email" not in data or not data["email"]:
-                return missing_data("email", 400)
+                return "Failed to load account JSON", 400
+            for key in keys:
+                if key not in data or not data[key]:
+                    return missing_data(key, 400)
             account.update(data)
             return "Account Updated", 200
         elif request.method == "DELETE":
+            keys = ["token", "email"]
             data = ""
             try:
                 data = json.loads(request.get_json())
             except:
-                return "Failed to load JSON object", 400
-            if "token" not in data or not data["token"]:
-                return missing_data("token", 400)
-            if "email" not in data or not data["email"]:
-                return missing_data("email", 400)
+                return "Failed to load account JSON", 400
+            for key in keys:
+                if key not in data or not data[key]:
+                    return missing_data(key, 400)
             return "Account Deleted", 200
-
     return app
