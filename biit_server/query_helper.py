433c4225a41dec7b7226730e0600939084006628
Lucas <lucashahn@comcast.net>
2020-09-22 15:30:56 -0400

    Removed unneeded authentication

--- biit_server/query_helper.py
+++ biit_server/query_helper.py
@@ -1,5 +1,6 @@
 from .http_responses import http200, http400
 
+
 def validate_body(body, fields):
     """
     Validates that the given fields exist in the body
@@ -18,14 +19,3 @@ def validate_query_params(query_params, fields):
         if field not in query_params:
             return http400(f"Missing query parameter {field}")
     return http200()
-
-def authenticate_token(token):
-        #TODO DB method for verifying token
-        #account.auth(token)
-
-    return True
-
-def authenticate_community(email,token):
-        #TODO DB method for verifying admin in community
-        #account.auth(email,token)
-    return True
