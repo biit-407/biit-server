0f0f2042ab895f20a2ab69d28d6e32165489c370
Ryan Chen <ryanjchen2@gmail.com>
2020-09-22 15:42:22 -0400

    Started database library

---
+++ biit_server/database.py
@@ -0,0 +1,9 @@
+from google.cloud import firestore
+
+def add(obj):
+    """Helper function to add object into database"""
+    return ""
+
+def update(id, update_data):
+    """Helper function to update objects in the database."""
+    return ""
