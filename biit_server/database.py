9598a1f250fb50d13a186b0c0de393243ed250ba
Ryan Chen <ryanjchen2@gmail.com>
2020-09-22 15:42:57 -0400

    Linter changes

--- biit_server/database.py
+++ biit_server/database.py
@@ -1,8 +1,10 @@
 from google.cloud import firestore
 
+
 def add(obj):
     """Helper function to add object into database"""
     return ""
+
 
 def update(id, update_data):
     """Helper function to update objects in the database."""
