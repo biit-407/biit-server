3940f067a228e01b3988ec0b4cdd697fb86cc6e1
Ryan Chen <ryanjchen2@gmail.com>
2020-09-23 11:47:40 -0400

    Added functions to database

--- biit_server/database.py
+++ biit_server/database.py
@@ -1,11 +1,20 @@
 from google.cloud import firestore
 
 
-def add(obj):
+def add(obj, database):
     """Helper function to add object into database"""
     return ""
 
 
-def update(id, update_data):
+def update(id, database, update_data):
     """Helper function to update objects in the database."""
+    return ""
+
+
+def remove(id, database):
+    """Helper function to remove objects in the database"""
+    return ""
+
+def get(id, database, query=None):
+    """Helper function to query the database"""
     return ""
