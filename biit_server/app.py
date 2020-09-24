90f955671412986b36d14ad9f2a8ddbc0c21f819
Lucas <lucashahn@comcast.net>
2020-09-24 13:00:24 -0400

    Ban Route added

--- biit_server/app.py
+++ biit_server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import json
 from .account_handler import account_post, account_get, account_put, account_delete
+from .ban_handler import ban_post, ban_put
 from .community_handler import (
     community_delete,
     community_get,
@@ -39,5 +40,13 @@ def create_app():
 
         elif request.method == "DELETE":
             return community_delete(request)
+
+    @app.route("/ban", methods=["POST", "PUT"])
+    def ban_route():
+        if request.method == "POST":
+            return ban_post(request)
+
+        elif request.method == "PUT":
+            return ban_put(request)
 
     return app
