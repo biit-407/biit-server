69d8e65df8ff12677a85d86681466be294a0762f
Stephen Davis <davi1138@purdue.edu>
2020-09-24 19:51:52 +0000

    Added tenant id

--- biit_server/azure.py
+++ biit_server/azure.py
@@ -2,14 +2,15 @@ import requests
 from typing import Tuple
 
 CLIENT_ID = "c128fe76-dc54-4daa-993c-1a13c1e82080"
-REDIRECT_URI = "TODO"
+TENANT_ID = "4130bd39-7c53-419c-b1e5-8758d6d63f21"
+REDIRECT_URI = "https://auth.expo.io/@stephend17/biit-mobile"
 
 def azure_refresh_token(refresh_token: str) -> Tuple[str, str]:
     """
     Refreshes a given refresh token and returns the
     new access token and refresh token
     """
-    url = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
+    url = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"
 
     payload = f'client_id={CLIENT_ID}&scope=https://graph.microsoft.com/User.Read&redirect_uri={REDIRECT_URI}&grant_type=refresh_token&refresh_token={refresh_token}'
     headers = {
