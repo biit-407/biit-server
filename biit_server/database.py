eb1dda9e8e9de570c556586bf6d3640897c75f2d
Ryan Chen <ryanjchen2@gmail.com>
2020-09-24 15:26:04 -0400

    Added some docstrings

--- biit_server/database.py
+++ biit_server/database.py
@@ -1,33 +1,67 @@
+from typing import Any, Dict, List
+
 from google.cloud import firestore
 
 
 class Database:
-    def __init__(self, collection, firestore=None):
+    def __init__(self, collection, firestore=None) -> None:
+        """The constructor for the Database class. Used to instantiate a Database reference object.
+
+        Args:
+            collection (str): the name of the collection
+            firestore (google.cloud.firestore.client): A Firestore client object, or a mock test object.
+
+        Returns:
+            None
+        """
         super().__init__()
         self.collection_name = collection
         self.firestore = firestore if firestore is not None else firestore.client()
         self.collection_ref = self.firestore.collection(self.collection_name)
 
-    def add(self, obj, id=None) -> True:
-        """Helper function to add object into database"""
+    def add(self, obj, id=None) -> Boolean:
+        """Helper function to add object into the database.
+
+        Args:
+            obj (Dict[str, Any]): A Dictionary containing the data you want to insert.
+            id (int, str): An identifying string or int. Optional.
+
+        Returns:
+            boolean, True if the document is successfully added, False if there was an error.
+        """
         try:
-            self.collection_ref.add(obj, id)
+            self.collection_ref.add(obj, document_id=id)
             return True
         except Exception:
             return False
 
-    def get(self, id):
-        """Helper function to get documents from the database"""
+    def get(self, id) -> Dict[str, Any]:
+        """Helper function to get documents from the database.
+
+        Args:
+            id (int, str): An identifying string or int.
+
+        Returns:
+            Dict[str, Any] if the document is successfully added. Boolean value of False if there was an error.
+        """
         try:
             results = self.collection_ref.document(id).get()
             return results
         except Exception:
             return False
 
-    def query(self, attr, operation, value):
-        """Helper function to query documents based on parameters"""
+    def query(self, field, operation, value) -> List[Dict[str, Any]]:
+        """Helper function to query documents based on parameters.
+
+        Args:
+            field (str): The field that you want to query on.
+            operation (str): Can be "==", ">=", "<=". Find out more at this link: https://googleapis.dev/python/firestore/latest/query.html#google.cloud.firestore_v1.query.Query.where
+            value (str): The value you are comparing to.
+        Returns:
+            List[Dict[str, Any]] if there are no errors. Boolean value of False if there is an error.
+        """
         try:
-            results = self.collection_ref.where(attr, operation, value).stream()
+            results = self.collection_ref.where(field, operation, value).stream()
             return [value for value in results]
         except Exception:
             return False
