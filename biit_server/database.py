359f56c7bbb3bf7264af48dc90982b7312ab5b04
Ryan Chen <ryanjchen2@gmail.com>
2020-09-24 12:58:35 -0400

    Finished databse library

--- biit_server/database.py
+++ biit_server/database.py
@@ -1,20 +1,33 @@
 from google.cloud import firestore
 
 
-def add(obj, database):
-    """Helper function to add object into database"""
-    return ""
+class Database:
+    def __init__(self, collection, firestore=None):
+        super().__init__()
+        self.collection_name = collection
+        self.firestore = firestore if firestore is not None else firestore.client()
+        self.collection_ref = self.firestore.collection(self.collection_name)
 
+    def add(self, obj, id=None) -> True:
+        """Helper function to add object into database"""
+        try:
+            self.collection_ref.add(obj, id)
+            return True
+        except Exception:
+            return False
 
-def update(id, database, update_data):
-    """Helper function to update objects in the database."""
-    return ""
+    def get(self, id):
+        """Helper function to get documents from the database"""
+        try:
+            results = self.collection_ref.document(id).get()
+            return results
+        except Exception:
+            return False
 
-
-def remove(id, database):
-    """Helper function to remove objects in the database"""
-    return ""
-
-def get(id, database, query=None):
-    """Helper function to query the database"""
-    return ""
+    def query(self, attr, operation, value):
+        """Helper function to query documents based on parameters"""
+        try:
+            results = self.collection_ref.where(attr, operation, value).stream()
+            return [value for value in results]
+        except Exception:
+            return False
