887fa216839768b331b808c76788488225d8cf11
Lucas <lucashahn@comcast.net>
2020-10-15 15:50:36 -0400

    Fixed Assertion Issue

--- tests/test_query.py
+++ tests/test_query.py
@@ -16,6 +16,7 @@ def test_invalid_fields():
         == "Bad Request: Not a valid update field: Good"
     )
 
+
 def test_empty_updatefields():
     valid = {"updateFields": ""}
     testfields = ["test", "Bad"]
@@ -23,9 +24,11 @@ def test_empty_updatefields():
         validate_update_field(valid, testfields)[0] == "Bad Request: Update Field empty"
     )
 
+
 def test_empty_fields():
     valid = {"updateFields": '{"test": "test", "Good": "Good"}'}
     testfields = []
     assert (
-        validate_update_field(valid, testfields)[0] == "Bad Request: Update Field empty"
+        validate_update_field(valid, testfields)[0]
+        == "Bad Request: Fields not being set"
     )
