1228491eaad97aa91f611c38aae02fcf8dbc04ec
Stephen Davis <davi1138@purdue.edu>
2020-10-20 10:31:17 -0400

    Added authenticated decorator

---
+++ biit_server/authentication.py
@@ -0,0 +1,56 @@
+from biit_server.http_responses import http400
+from biit_server.azure import azure_refresh_token
+from enum import Enum
+
+
+class AuthenticatedType(Enum):
+    BODY = 0
+    """
+    the token to authenticate with is located in the body 
+    of the request
+    """
+    QUERY = 1
+    """
+    the token to authenticate with is located in the query
+    parameters of the request
+    """
+    NONE = 2
+    """
+    there is no authentication for this request
+    """
+
+
+def authenticated(type: AuthenticatedType = AuthenticatedType.NONE):
+    """
+    Decorator for making sure a request is authenticated properly
+
+    Args:
+        type (AuthenticatedType): the type of authentication to use
+
+    """
+
+    def decorator(func):
+        def wrapper(request):
+            auth = None
+            if type == AuthenticatedType.BODY:
+                body = None
+                try:
+                    body = request.get_json()
+                except:
+                    return http400("Missing body")
+                auth = azure_refresh_token(body["token"])
+                print(auth, body)
+                if not auth[0]:
+                    return http400("Not Authenticated")
+            if type == AuthenticatedType.QUERY:
+                args = request.args
+                auth = azure_refresh_token(args["token"])
+                if not auth[0]:
+                    return http400("Not Authenticated")
+
+            result = func(request, auth)
+            return result
+
+        return wrapper
+
+    return decorator
