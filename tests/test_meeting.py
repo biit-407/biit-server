b28ba637a463992e39d7a8a89f90fabfca1f1d34
fatcat2 <ryanjchen2@gmail.com>
2020-11-01 20:51:05 -0500

    Added support for getting pending meetings

--- tests/test_meeting.py
+++ tests/test_meeting.py
@@ -590,4 +590,51 @@ def test_meeting_get_all(client):
         assert return_data["refresh_token"] == "RefreshToken"
         assert return_data["status_code"] == 200
 
-        # instance.query.assert_called_once_with(test_json["id"], "array_in", "user_list")
+
+def test_meeting_get_pending_all(client):
+    """
+    Tests that getting all meetings that are pending invits works correctly
+    """
+    with patch("biit_server.meeting_handler.Database") as mock_database, patch(
+        "biit_server.meeting_handler.Meeting"
+    ) as mock_meeting:
+        instance = mock_database.return_value
+        instance.collection_ref.get.return_value = [1, 1, 1]
+
+        query_data = {"email": "beidou@purdue.edu", "token": "dabonem"}
+
+        test_json = {
+            "id": "random_meeting",
+            "user_list": {"beidou@purdue.edu": 0},
+            "duration": 110,
+            "location": "Li Yue",
+            "meettype": "Gacha",
+            "timestamp": "noon",
+        }
+
+        mock_meeting.return_value = Meeting(
+            id=test_json["id"],
+            user_list=test_json["user_list"],
+            duration=test_json["duration"],
+            location=test_json["location"],
+            meeting_type=test_json["meettype"],
+            timestamp=test_json["timestamp"],
+        )
+
+        rv = client.get(
+            "/meeting/pending",
+            query_string=query_data,
+            follow_redirects=True,
+        )
+
+        return_data = json.loads(rv.data.decode())
+
+        assert return_data["access_token"] == "AccessToken"
+        assert len(return_data["data"]) == 3
+        assert return_data["data"][0]["timestamp"] == test_json["timestamp"]
+        assert return_data["data"][0]["location"] == test_json["location"]
+        assert return_data["data"][0]["meettype"] == test_json["meettype"]
+        assert return_data["data"][0]["user_list"] == test_json["user_list"]
+        assert return_data["message"] == "Meetings retrieved"
+        assert return_data["refresh_token"] == "RefreshToken"
+        assert return_data["status_code"] == 200
