3e01960c35c9dc804c8a1f7e1ab1e69182dba7fc
fatcat2 <ryanjchen2@gmail.com>
2020-11-04 22:41:37 -0500

    Taking out the extra rating_handler function

--- biit_server/rating_handler.py
+++ biit_server/rating_handler.py
@@ -62,58 +62,6 @@ def rating_post(request, auth):
     return jsonHttp200("Rating created", response)
 
 
-@validate_fields(["user", "token"], ValidateType.BODY)
-@authenticated(AuthenticatedType.BODY)
-def rating_post(request, auth):
-    """Handles the rating POST endpoint
-    Validates the keys in the request then calls the database to get all unrated meetups
-    Args:
-        request: A request object that contains a json object with keys: user, token
-
-    Returns:
-        (json): Http 200 string response containing the  refresh token and new token
-
-    Raises:
-        Http 400 when the json is missing a key
-    """
-    body = request.get_json()
-
-    rating_db = Database("ratings")
-
-    rating_snapshot = rating_db.get(body["meeting_id"])
-
-    if rating_snapshot == False:
-        rating = Rating(
-            meeting_id=body["meeting_id"], rating_dict={body["user"]: body["rating"]}
-        )
-
-        success = rating_db.add(rating.to_dict(), id=body["meeting_id"])
-
-    else:
-        rating = Rating(document_snapshot=rating_snapshot)
-        try:
-            rating.set_rating(body["user"], body["rating"])
-        except RatingAlreadySetException:
-            return http400(f"Rating for user {body['user']} has already been set")
-
-        success = rating_db.update(
-            body["meeting_id"], {"rating_dict": rating.get_ratings()}
-        )
-
-    if not success:
-        return http400(
-            f"Error in updating meeting id {body['meeting_id']} in Firestore database."
-        )
-
-    response = {
-        "access_token": auth[0],
-        "refresh_token": auth[1],
-        "data": rating.to_dict(),
-    }
-
-    return jsonHttp200("Rating created", response)
-
-
 @validate_fields(["meeting_id", "token"], ValidateType.QUERY)
 @authenticated(AuthenticatedType.QUERY)
 def rating_get(request, auth):
